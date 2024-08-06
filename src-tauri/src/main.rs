ffebc749f9ab3b75bee81385b42267330e403018
Kalos Lazo <lzm.kalos@gmail.com>
2024-08-06 13:12:43 -0500

    feat: tauri dotfile implementation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{command, Window};
 use std::process::{Command};
+use std::env;
 
-#[tauri::command]
-fn install_packages(window: tauri::Window, packages: Vec<String>) -> Result<String, String> {
+#[command]
+fn install_packages(window: Window, packages: Vec<String>) -> Result<String, String> {
     if packages.is_empty() {
         return Err("Error, no packages provided for installation.".to_string());
     }
@@ -37,9 +39,35 @@ fn install_packages(window: tauri::Window, packages: Vec<String>) -> Result<Stri
     Ok("Packages installed with 0 errors.".to_string())
 }
 
+#[command]
+fn execute_script(script_name: String) -> Result<String, String> {
+    let current_dir = env::current_dir().map_err(|e| e.to_string())?;
+    let script_path = current_dir.join("../src/scripts").join(format!("{}.sh", script_name));
+    let script_path = script_path.canonicalize().map_err(|e| e.to_string())?;
+
+    // Ejecutar el script con bash
+    let output = std::process::Command::new("bash")
+        .arg(script_path)
+        .output();
+
+    match output {
+        Ok(output) => {
+            if output.status.success() {
+                Ok(String::from_utf8_lossy(&output.stdout).to_string())
+            } else {
+                Err(String::from_utf8_lossy(&output.stderr).to_string())
+            }
+        },
+        Err(e) => Err(e.to_string()),
+    }
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![install_packages])
+        .invoke_handler(tauri::generate_handler![
+            install_packages,
+            execute_script
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }   
--- src/helpers/InfoDotfile.vue
+++ src/helpers/InfoDotfile.vue
@@ -11,17 +11,34 @@
       </div>
     </div>
   </div>
+  <HandleErrorMessage v-if="show_error" :message="msg_error"/>
 </template>
 
 <script setup lang="ts">
+import { invoke } from '@tauri-apps/api';
 import { ref, defineProps, defineEmits } from 'vue';
+import HandleErrorMessage from './HandleErrorMessage.vue';
 
 const emit = defineEmits(['close']);
 
 const props = defineProps<{ name: string }>()
 const completedInstallation = ref(false);
 const installing = ref(false);
+const show_error = ref(false);
+const msg_error = ref('');
 
-const install_dotfiles = () => {}
 const handle_close = () => emit('close');
+const install_dotfiles = async () => {
+  installing.value = true;
+  try {
+    const result = await invoke('execute_script', { scriptName: props.name });
+    console.log('Script output:', result);
+    completedInstallation.value = true;
+  } catch (error) {
+    show_error.value = true;
+    msg_error.value = error as string;
+  } finally {
+    installing.value = false;
+  };
+};
 </script>
